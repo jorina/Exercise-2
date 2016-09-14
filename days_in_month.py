6eaede49bb371be1fab88c52dc986352b65b1868
hannrant <hanna.rantanen@helsinki.fi>
2016-09-14 22:33:27 +0300

    days_in_month

---
+++ days_in_month.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 14 20:50:30 2016
+
+@author: Hanna
+"""
+
+#SelectedMonth
+SelectedMonth = 1
+
+#Months
+Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+#Days
+Days = ['31', '28', '31', '30,' '31', '30', '31', '31', '30', '31', '30', '31']
+
+
+#Print the number of days in SelectedMonth
+print("Number of days in", Months[SelectedMonth], "is", Days[SelectedMonth])
